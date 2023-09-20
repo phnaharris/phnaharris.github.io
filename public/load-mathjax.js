7946de471a83ed110df16a023978e2b4bd034527
phnaharris <phnanh.harris@gmail.com>
2023-09-20 23:07:24 +0700

    feat(mathjax): not best choice, but good enough

---
+++ public/load-mathjax.js
@@ -0,0 +1,15 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [
+      ["$", "$"],
+      ["\\(", "\\)"],
+    ],
+  },
+};
+
+(function () {
+  var script = document.createElement("script");
+  script.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js";
+  script.async = true;
+  document.head.appendChild(script);
+})();
